747c118a4cc0fb37ac9607b4574b2ebd51598320
Arya <aryailham05@users.github.com>
2020-05-03 04:53:20 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,10 +9,10 @@ class AuthController extends Controller
 {
     public function clientLogin(Request $request){
         // dd($request->all());
-        if(Auth::attempt($request->only('username','password'))){
-            return redirect('/clienthome');
+        if(Auth::attempt($request->only('client_email','client_password'))){
+            return view('clienthome');
         }
-        return redirect('/login');
+        return redirect('/clientlogin');
     }
 
     public function logout(){
