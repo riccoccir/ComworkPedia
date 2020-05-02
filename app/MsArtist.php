839778d889c35c834ab74d0420883146ce192147
riccoccir <52000062+riccoccir@users.noreply.github.com>
2020-05-02 19:00:25 +0700

    add Ms Artist Model, Factory, Seeder

---
+++ app/MsArtist.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MsArtist extends Model
+{
+    protected $table = 'msartist';
+}
---
+++ database/factories/MsArtistFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\MsArtist;
+use Faker\Generator as Faker;
+
+$factory->define(MsArtist::class, function (Faker $faker) {
+    return [
+        'artist_name'=> $faker->firstname(),
+        'artist_email' => $faker->email(),
+        'artist_password'=> $faker->password(),
+        'artist_phoneNumber' => $faker->phoneNumber(),
+        'artist_instagram'=> $faker->userName(),
+        'artist_webiste' => $faker->domainName()
+    ];
+});
