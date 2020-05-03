125fb3c14c2a58598684afedb5e160165a6e41a8
Arya <aryailham05@users.github.com>
2020-05-03 11:35:26 +0700

    pecah AuthController jadi client dan artist controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,38 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
-use App\MsClient;
 use App\MsArtist;
 
-class AuthController extends Controller
+class ArtistController extends Controller
 {
-    public function clientLogin(Request $request){
-        dd($request->all());
-        if(Auth::attempt($request->only('client_email','client_password'))){
-            return view('clienthome');
-        }
-        return redirect('/clientlogin');
-    }
-
-    public function clientRegister(Request $request){  
-        $this->validate($request, [
-            'username' => 'required',
-            'firstname' => 'required',
-            'lastname' => 'required',
-            'email' => 'required|email|unique:msclient,client_email',
-            'phonenumber' => 'required',
-            'password' => 'required|min:8|same:retypepassword'
-        ]);
-        
-        MsClient::create([
-            'client_name' => $request ->username,
-            'client_email' => $request -> email,
-            'client_password' => bcrypt($request -> password),
-            'client_phoneNumber' => $request ->phonenumber
-        ]);
-
-        return redirect('/clientlogin');
-    }
 
     public function artistLogin(){
         
