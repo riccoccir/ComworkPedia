7c1aef4159f05d9099af5005fd06bdf032e542a5
riccoccir <52000062+riccoccir@users.noreply.github.com>
2020-05-10 22:36:25 +0700

    query db kelar

--- app/Http/Controllers/HireController.php
+++ app/Http/Controllers/HireController.php
@@ -1,12 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\TrCommission;
+use App\MsArtist;
+use App\MsCommissionType;
+use DB;
 use Illuminate\Http\Request;
 
 class HireController extends Controller
 {
-    public function index(){
+    public function index($typeid,$commissionid){
+        $data1 = DB::table('TrCommission')
+        ->join('MsArtist','TrCommission.artist_id','=','MsArtist.id')
+        ->join('MsCommissionType','TrCommission.commission_type_id','=','MsCommissionType.commission_type_id')
+        ->where('TrCommission.commission_id','=',$commissionid)
+        ->get();
+
+        // dd($data1);
         return view('hire');
     }
 }
